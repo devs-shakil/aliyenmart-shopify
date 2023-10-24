ce9de79df00af90450000564021648f8c17de3be
shakilwebmaster <mdshakil102036@gmail.com>
2023-10-24 23:38:14 +0600

    shopify liquid practice

---
+++ assets/aliyenmart.js
@@ -0,0 +1,16 @@
+// collection slider 
+// $(document).ready(function(){
+//     $('.collection-wrapper').slick({
+//         slidesToShow: 5,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         fade: true
+//     });
+//   });
+
+  const test = document.querySelector('.collection-wrapper')
+  console.log(test)
