6f9642f16e7edafc701c8085cee634fc50a05562
shakilwebmaster <mdshakil102036@gmail.com>
2023-10-28 23:01:51 +0600

    product page developement

--- assets/aliyenmart.js
+++ assets/aliyenmart.js
@@ -1,16 +1,7 @@
-// collection slider 
-// $(document).ready(function(){
-//     $('.collection-wrapper').slick({
-//         slidesToShow: 5,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         dots: true,
-//         infinite: true,
-//         speed: 500,
-//         fade: true
-//     });
-//   });
+const $button  = document.querySelector('#sidebar-toggle');
+const $wrapper = document.querySelector('#wrapper');
 
-  const test = document.querySelector('.collection-wrapper')
-  console.log(test)
+$button.addEventListener('click', (e) => {
+  e.preventDefault();
+  $wrapper.classList.toggle('toggled');
+});
